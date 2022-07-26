84b0388c1e6be18d121c8169e0e6509a55280e18
Kazuichi Takashiro <takashiro@qq.com>
2022-07-27 00:18:00 +0800

    Add DiffViewer

---
+++ src/gui/DiffViewer/DiffViewer.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+
+import {
+	TestReport,
+	TestStatus,
+} from '@pixdif/model';
+
+import Client from '../../api/Client';
+import Clickable from '../../base/Clickable';
+import { makeToast } from '../../base/Toast';
+
+import DiffLayout from '../DiffLayout';
+
+import './DiffViewer.scss';
+
+interface DiffViewerProps {
+	report: TestReport;
+	caseId: number;
+}
+
+function getCurrentDir(): string {
+	if (window.location.protocol !== 'file:') {
+		return '.';
+	}
+
+	const baseUrl = window.location.href.substring(0, window.location.href.lastIndexOf('/'));
+	const myDir = baseUrl.startsWith('file://') ? baseUrl.substring(7) : baseUrl;
+	if (myDir.match(/^\/\w+:/)) {
+		return myDir.substring(1);
+	}
+	return myDir;
+}
+
+export default function DiffViewer(props: DiffViewerProps): JSX.Element {
+	const {
+		report,
+		caseId,
+	} = props;
+	const { config } = report;
+
+	const [matchedHidden, setMatchedHidden] = React.useState(true);
+
+	const toggleMatched = (): void => {
+		setMatchedHidden((prev) => !prev);
+	};
+
+	const updateBaseline = async (): Promise<void> => {
+		const data = report.testCases[caseId];
+		const client = new Client(config.wsEndpoint);
+		try {
+			await client.connect();
+			const currentDir = getCurrentDir();
+			const res = await client.updateBaseline(`${currentDir}/${data.baseline}`, `${currentDir}/${data.actual}`);
+			makeToast(res.message, res.level);
+		} catch (error) {
+			makeToast('Failed to connect to the server. Please run `node server`', 'error');
+		} finally {
+			client.disconnect();
+		}
+	};
+
+	const threshold = config.diffThreshold || 0;
+	const data = report.testCases[caseId];
+	const diffs = (data.diffs || []).map((ratio, i) => ({ index: i + 1, ratio }));
+
+	const imageDir = `image/${data.path.replace(/\.[^/\\.]+$/, '')}`;
+	const thresholdText = (threshold * 100).toFixed(3);
+
+	return (
+		<div className="diff-viewer">
+			<h2>{data.actual}</h2>
+			<div className="control-panel">
+				<input
+					type="checkbox"
+					id="hide-matched"
+					checked={matchedHidden}
+					onChange={toggleMatched}
+				/>
+				<label htmlFor="hide-matched">
+					Hide matched pages
+					<span className="threshold">{thresholdText}</span>
+				</label>
+			</div>
+			<h3 className="composed-layout">
+				<p><a href={data.baseline} target="_blank" rel="noreferrer">Baseline</a></p>
+				<p>Difference</p>
+				<p><a href={data.actual} target="_blank" rel="noreferrer">Output</a></p>
+			</h3>
+			<ul className="page-list">
+				{diffs.map(({ index, ratio }) => {
+					if (ratio <= threshold && matchedHidden) {
+						return null;
+					}
+
+					let className;
+					if (!matchedHidden) {
+						className = ratio <= threshold ? 'matched' : 'unmatched';
+					}
+					const key = `${matchedHidden ? 'sliced-' : 'origin-'}${index}`;
+					const anchorName = `page${index}`;
+					const title = `Page ${index}`;
+					const ratioText = (ratio * 100).toFixed(3);
+					return (
+						<li key={key} className={className}>
+							<h4>
+								<span id={anchorName}>{title}</span>
+								<em className="ratio">{ratioText}</em>
+							</h4>
+							<DiffLayout
+								path={imageDir}
+								pageIndex={index}
+							/>
+						</li>
+					);
+				})}
+			</ul>
+			{(data.status === TestStatus.BaselineNotFound || data.status === TestStatus.Mismatched) && (
+				<div className="button-area">
+					<Clickable
+						component="button"
+						type="button"
+						onClick={updateBaseline}
+					>
+						{data.status === TestStatus.BaselineNotFound ? 'Add Baseline' : 'Update Baseline'}
+					</Clickable>
+				</div>
+			)}
+		</div>
+	);
+}

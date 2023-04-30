224ba991e121cdc9b691a876e284c97338d8a3fa
Kazuichi Takashiro <takashiro@qq.com>
2023-04-30 23:27:34 +0800

    Add disabled state to Clickable

--- src/base/Clickable.tsx
+++ src/base/Clickable.tsx
@@ -3,21 +3,29 @@ import { isModifierKeyPressed, isTriggered } from '../util/keyboard';
 
 interface ClickableProps<T extends HTMLElement = HTMLDivElement> extends React.HTMLAttributes<T> {
 	component?: React.ElementType;
+	disabled?: boolean;
 	onTrigger(event: React.SyntheticEvent<T>): void;
 	innerRef?: React.ForwardedRef<T>;
 }
 
-export function Clickable<T extends HTMLElement = HTMLDivElement>({
-	innerRef,
-	component: Component = 'div',
-	role = 'button',
-	tabIndex = 0,
-	onTrigger,
-	onClick,
-	onKeyDown,
-	children,
-	...otherProps
-}: ClickableProps<T>): JSX.Element {
+export function Clickable<T extends HTMLElement = HTMLDivElement>(
+	props: ClickableProps<T>,
+): JSX.Element {
+	const {
+		innerRef,
+		component: Component = 'div',
+		role = 'button',
+		disabled,
+		onTrigger,
+		onClick,
+		onKeyDown,
+		children,
+		...otherProps
+	} = props;
+	const {
+		tabIndex = disabled ? -1 : 0,
+	} = props;
+
 	const handleClick = React.useCallback((e: React.MouseEvent<T>) => {
 		onTrigger(e);
 		onClick?.(e);
@@ -35,8 +43,9 @@ export function Clickable<T extends HTMLElement = HTMLDivElement>({
 			ref={innerRef}
 			role={role}
 			tabIndex={tabIndex}
-			onClick={handleClick}
-			onKeyDown={handleKeyDown}
+			onClick={disabled ? undefined : handleClick}
+			onKeyDown={disabled ? undefined : handleKeyDown}
+			aria-disabled={disabled}
 			{...otherProps}
 		>
 			{children}

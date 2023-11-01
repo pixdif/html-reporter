19038fb505523aa68d6748c37495a4170e0bac90
Kazuichi Takashiro <takashiro@qq.com>
2023-11-01 22:28:49 +0800

    Remove `react` and `react-dom`

--- src/gui/util/keyboard.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function isModifierKeyPressed(e: React.KeyboardEvent): boolean {
-	return e.altKey || e.ctrlKey || e.shiftKey || e.metaKey;
-}
-
-export function isTriggered(e: React.KeyboardEvent): boolean {
-	return e.key === ' ' || e.key === 'Enter';
-}
